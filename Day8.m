69fc034654b6941e1dd4d43f4ae7d38216a35f50
SungHCho <94133896+SungHCho@users.noreply.github.com>
2021-12-08 13:42:06 -0800

    Day 8: Major optimization using intersect with 1&4

--- Day8.m
+++ Day8.m
@@ -2,16 +2,16 @@
 % You barely reach the safety of the cave when the whale smashes into the
 % cave mouth, collapsing it. Sensors indicate another exit to this cave at
 % a much greater depth, so you have no choice but to press on.
-% 
+%
 % As your submarine slowly makes its way through the cave system, you
 % notice that the four-digit seven-segment displays in your submarine are
 % malfunctioning; they must have been damaged during the escape. You'll be
 % in a lot of trouble without them, so you'd better figure out what's
 % wrong.
-% 
+%
 % Each digit of a seven-segment display is rendered by turning on or off
 % any of seven segments named a through g:
-% 
+%
 %   0:      1:      2:      3:      4:
 %  aaaa    ....    aaaa    aaaa    ....
 % b    c  .    c  .    c  .    c  b    c
@@ -20,7 +20,7 @@
 % e    f  .    f  e    .  .    f  .    f
 % e    f  .    f  e    .  .    f  .    f
 %  gggg    ....    gggg    gggg    ....
-% 
+%
 %   5:      6:      7:      8:      9:
 %  aaaa    aaaa    aaaa    aaaa    aaaa
 % b    .  b    .  .    c  b    c  b    c
@@ -32,32 +32,32 @@
 % So, to render a 1, only segments c and f would be turned on; the rest
 % would be off. To render a 7, only segments a, c, and f would be turned
 % on.
-% 
+%
 % The problem is that the signals which control the segments have been
 % mixed up on each display. The submarine is still trying to display
 % numbers by producing output on signal wires a through g, but those wires
 % are connected to segments randomly. Worse, the wire/segment connections
 % are mixed up separately for each four-digit display! (All of the digits
 % within a display use the same connections, though.)
-% 
+%
 % So, you might know that only signal wires b and g are turned on, but that
 % doesn't mean segments b and g are turned on: the only digit that uses two
 % segments is 1, so it must mean segments c and f are meant to be on. With
 % just that information, you still can't tell which wire (b/g) goes to
 % which segment (c/f). For that, you'll need to collect more information.
-% 
+%
 % For each display, you watch the changing signals for a while, make a note
 % of all ten unique signal patterns you see, and then write down a single
 % four digit output value (your puzzle input). Using the signal patterns,
 % you should be able to work out which pattern corresponds to which digit.
-% 
+%
 % For example, here is what you might see in a single entry in your notes:
-% 
+%
 % acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
 % cdfeb fcadb cdfeb cdbaf
 % (The entry is wrapped here to two lines so it fits; in your notes, it
 % will all be on a single line.)
-% 
+%
 % Each entry consists of ten unique signal patterns, a | delimiter, and
 % finally the four digit output value. Within an entry, the same
 % wire/segment connections are used (but you don't know what the
@@ -68,15 +68,15 @@
 % lines d, a, and b are on. Because 4 is the only digit that uses four
 % segments, eafb means that to render a 4, signal lines e, a, f, and b are
 % on.
-% 
+%
 % Using this information, you should be able to work out which combination
 % of signal wires corresponds to each of the ten digits. Then, you can
 % decode the four digit output value. Unfortunately, in the above example,
 % all of the digits in the output value (cdfeb fcadb cdfeb cdbaf) use five
 % segments and are more difficult to deduce.
-% 
+%
 % For now, focus on the easy digits. Consider this larger example:
-% 
+%
 % be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
 % fdgacbe cefdb cefbgd gcbe
 % edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
@@ -102,7 +102,7 @@
 % those digits. Counting only digits in the output values (the part after |
 % on each line), in the above example, there are 26 instances of digits
 % that use a unique number of segments (highlighted above).
-% 
+%
 % In the output values, how many times do digits 1, 4, 7, or 8 appear?
 
 clc;
@@ -121,12 +121,12 @@ fprintf('1, 4, 7, or 8 appears %i times in the output.\n',count);
 % --- Part Two ---
 % Through a little deduction, you should now be able to determine the
 % remaining digits. Consider again the first example above:
-% 
+%
 % acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
 % cdfeb fcadb cdfeb cdbaf
 % After some careful analysis, the mapping between signal wires and
 % segments only make sense in the following configuration:
-% 
+%
 %  dddd
 % e    a
 % e    a
@@ -135,7 +135,7 @@ fprintf('1, 4, 7, or 8 appears %i times in the output.\n',count);
 % g    b
 %  cccc
 % So, the unique signal patterns would correspond to the following digits:
-% 
+%
 % acedgfb: 8
 % cdfbe: 5
 % gcdfa: 2
@@ -147,16 +147,16 @@ fprintf('1, 4, 7, or 8 appears %i times in the output.\n',count);
 % cagedb: 0
 % ab: 1
 % Then, the four digits of the output value can be decoded:
-% 
+%
 % cdfeb: 5
 % fcadb: 3
 % cdfeb: 5
 % cdbaf: 3
 % Therefore, the output value for this entry is 5353.
-% 
+%
 % Following this same process for each entry in the second, larger example
 % above, the output value of each entry can be determined:
-% 
+%
 % fdgacbe cefdb cefbgd gcbe: 8394
 % fcgedb cgb dgebacf gc: 9781
 % cg cg fdcagb cbg: 1197
@@ -168,125 +168,77 @@ fprintf('1, 4, 7, or 8 appears %i times in the output.\n',count);
 % gbdfcae bgc cg cgb: 8717
 % fgae cfgab fg bagce: 4315
 % Adding all of the output values in this larger example produces 61229.
-% 
+%
 % For each entry, determine all of the wire/segment connections and decode
 % the four-digit output values. What do you get if you add up all of the
 % output values?
 
 keystr = string([DataIn{1:10}]);
 out = string([DataIn{11:14}]);
-key = '       ';
-output = zeros(size(keystr,1),1);
+output = 0;
 
 for i = 1:size(keystr,1)
     onechar = char(keystr(i,strlength(keystr(i,:))==2));
     fourchar = char(keystr(i,strlength(keystr(i,:))==4));
-    sevenchar = char(keystr(i,strlength(keystr(i,:))==3));
-    eightchar = 'abcdefg';
-    zerosixnine = char(keystr(i,strlength(keystr(i,:))==6));
-    key(1) = findkey(sevenchar, onechar);
-    sixchar = identifysix(zerosixnine, onechar);
-    key(3) = findkey(eightchar, sixchar);
-    key(6) = findkey(onechar, key(3));
-    twothreefive = char(keystr(i, strlength(keystr(i,:))==5));
-    fivechar = identifyfive(twothreefive, key(3));
-    key(5) = findkey(sixchar, fivechar);
-    ninechar = identifynine(zerosixnine, key(5));
-    zerochar = identifyzero(zerosixnine, sixchar, ninechar);
-    key(4) = findkey(eightchar, zerochar);
-    key(2) = findkey(fourchar, [key(3:4),key(6)]);
-    key(7) = findkey(eightchar, key(1:6));
     
-    output(i) = getoutput(out(i,:), key);
+    output = output+DeriveDigit(out(i,:), onechar, fourchar);
 end
 
-fprintf('Sum of all output values is %i.\n',sum(output))
+fprintf('Sum of all output values is %i.\n',output)
 
-function output = getoutput(out, key)
-    zerochar = sort([key(1:3) key(5:7)]);
-    onechar =  sort([key(3) key(6)]);
-    twochar =  sort([key(1) key(3:5) key(7)]);
-    threechar =  sort([key(1) key(3:4) key(6:7)]);
-    fourchar =  sort([key(2:4) key(6)]);
-    fivechar =  sort([key(1:2) key(4) key(6:7)]);
-    sixchar =  sort([key(1:2) key(4:7)]);
-    sevenchar =  sort([key(1) key(3) key(6)]);
-    eightchar = 'abcdefg';
-    ninechar =  sort([key(1:4) key(6:7)]);
-    
-    numbers = zeros(1,4);
-    
-    for i = 1:4
-        switch sort(char(out(i)))
-            case zerochar
-                numbers(i) = 0;
-            case onechar
-                numbers(i) = 1;
-            case twochar
-                numbers(i) = 2;
-            case threechar
-                numbers(i) = 3;
-            case fourchar
-                numbers(i) = 4;
-            case fivechar
-                numbers(i) = 5;
-            case sixchar
-                numbers(i) = 6;
-            case sevenchar
-                numbers(i) = 7;
-            case eightchar
-                numbers(i) = 8;
-            case ninechar
-                numbers(i) = 9;
+function output = DeriveDigit(key, onechar, fourchar)
+    output = 0;
+    keylength = length(key);
+    for i = 1:keylength
+        input = char(key(i));
+        charlength = length(input);
+        switch charlength
+            case 2
+                output = output+1*10^(keylength-i);
+                continue;
+            case 3
+                output = output+7*10^(keylength-i);
+                continue;
+            case 4
+                output = output+4*10^(keylength-i);
+                continue;
+            case 7
+                output = output+8*10^(keylength-i);
+                continue;
         end
-    end
-    output = numbers(1)*1000+numbers(2)*100+numbers(3)*10+numbers(4);
-end
-
-function keychar = findkey(char1, char2)
-    charmatch = false(size(char1));
-    for i = 1:length(char2)
-        charmatch = or(charmatch,char1 == char2(i));
-    end
-    keychar = char1(~charmatch);
-end
-
-function sixchar = identifysix(zerosixnine, onechar)
-    for i = 1:3
-        if ~contains(zerosixnine(1,:,i),onechar(1)) ||...
-                ~contains(zerosixnine(1,:,i),onechar(2))
-            sixchar = zerosixnine(1,:,i);
-            return;
+        
+        onecommon = findintersectnum(input,onechar);
+        fourcommon = findintersectnum(input,fourchar);
+        
+        if charlength == 5
+            if onecommon == 1
+                if fourcommon == 2
+                    output = output+2*10^(keylength-i);
+                else
+                    output = output+5*10^(keylength-i);
+                end
+            else
+                output = output+3*10^(keylength-i);
+            end
+        else
+            if onecommon == 2
+                if fourcommon == 3
+                    continue;
+                else
+                    output = output+9*10^(keylength-i);
+                end
+            else
+                output = output+6*10^(keylength-i);
+            end
         end
     end
 end
 
-
-function ninechar = identifynine(zerosixnine, Echar)
-    for i = 1:3
-        if ~contains(zerosixnine(1,:,i),Echar)
-            ninechar = zerosixnine(1,:,i);
-            return;
-        end
-    end
-end
-
-
-function zerochar = identifyzero(zerosixnine, sixchar, ninechar)
-    for i = 1:3
-        if ~(all(zerosixnine(1,:,i) == sixchar) ||...
-                all(zerosixnine(1,:,i) == ninechar))
-            zerochar = zerosixnine(1,:,i);
-            return;
-        end
-    end
-end
-
-function fivechar = identifyfive(twothreefive,Cchar)
-    for i = 1:3
-        if ~contains(twothreefive(1,:,i),Cchar)
-            fivechar = twothreefive(1,:,i);
-            return;
+function numchar = findintersectnum(input1, input2)
+    numchar = 0;
+    for i = 1:length(input2)
+        if any(input1 == input2(i))
+            numchar = numchar+1;
         end
     end
 end
